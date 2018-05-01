0ef73732c24348750581ac1615b0b109189815ff
yeureka <yeureka@yeurekadeMacBook-Pro.local>
2018-05-01 23:52:25 +0800

    v1.1 Increase the incremental mode

--- db_manager.py
+++ db_manager.py
@@ -2,7 +2,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.exc import OperationalError
 from sys import argv
 from os import path
 
